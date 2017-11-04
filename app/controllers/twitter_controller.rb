027b99f95f9786402253d0877b7ccd8f5f5ca7d3
Jesus Castello <matugm@gmail.com>
2017-11-04 20:31:58 +0100

    List top 10 tweets.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,7 +3,8 @@ class TwitterController < ApplicationController
   def analyze
     twitter    = TwitterAnalyzer.new(params[:user])
 
-    @user      = params[:user]
-    @followers = twitter.followers_count
+    @user       = params[:user]
+    @followers  = twitter.followers_count
+    @top_tweets = twitter.prepare_tweets.first(10)
   end
 end

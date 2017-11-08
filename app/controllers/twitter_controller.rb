6f61599d67f51d9822f2fdd7afcbd922bc67279e
Jesus Castello <matugm@gmail.com>
2017-11-08 21:48:27 +0100

    Check if Twitter user is valid.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,10 +2,14 @@
 class TwitterController < ApplicationController
   def analyze
     twitter    = TwitterAnalyzer.new(params[:user])
+    @user      = params[:user]
 
-    @user       = params[:user]
-    @followers  = twitter.followers_count
-    @top_tweets = twitter.prepare_tweets.first(10)
-    @word_count = twitter.get_word_analysis
+    if twitter.valid_user?
+      @followers  = twitter.followers_count
+      @top_tweets = twitter.prepare_tweets.first(10)
+      @word_count = twitter.get_word_analysis
+    else
+      render plain: "User invalid."
+    end
   end
 end
